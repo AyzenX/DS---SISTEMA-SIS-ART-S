73b18bff29a95e1fa4a654835d5c62aa002847a8
João Kaiky <jkaiky80@gmail.com>
2021-06-27 17:38:40 -0300

    receber venda

---
+++ ReceberVenda.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace projetoSISARTS
+{
+    /// <summary>
+    /// Lógica interna para ReceberVenda.xaml
+    /// </summary>
+    public partial class ReceberVenda : Window
+    {
+        public ReceberVenda()
+        {
+            InitializeComponent();
+        }
+    }
+}
