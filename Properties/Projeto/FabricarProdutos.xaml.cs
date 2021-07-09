3b1dfdad0e85ce5428f700cc077c3843422faf15
Sabrinafc <87147976+Sabrinafc@users.noreply.github.com>
2021-07-09 00:03:51 -0300

    Recuperação!

---
+++ Properties/Projeto/FabricarProdutos.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace projetoSISARTS
+{
+    /// <summary>
+    /// Lógica interna para FabricarProdutos.xaml
+    /// </summary>
+    public partial class FabricarProdutos : Window
+    {
+        public FabricarProdutos()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        public class Produtos
+        {
+            public String produtoID { get; set; }
+            public String produtoLinhaDeProducao { get; set; }
+            public String produtoQTD { get; set; }
+        }
+
+        private void Adicionar(object sender, RoutedEventArgs e)
+        {
+            String LinhaDeProducao = txNomeProduto.Text;
+            String ProdutoQTD = txQuantidade.Text;
+
+            Produtos produtos = new Produtos();
+
+            //Por hora não captura ID, pois deve o id atribuido do Cadastro de Produtos
+            produtos.produtoLinhaDeProducao = LinhaDeProducao;
+            produtos.produtoQTD = ProdutoQTD;
+
+            DataGridProdutos.Items.Add(produtos);
+        }
+
+        private void Sair(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Produzir(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Produtos adicionados à linha de produção");
+        }
+
+        private void Remover(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
---
+++ Properties/Projeto/ReceberVenda.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace projetoSISARTS
+{
+    /// <summary>
+    /// Lógica interna para ReceberVenda.xaml
+    /// </summary>
+    public partial class ReceberVenda : Window
+    {
+        public ReceberVenda()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Deseja cancelar o pagamento da despesa ??", "Cancelar Pagamento", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    this.Close();
+                    break;
+            }
+        }
+
+        private void btnPagar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("A Venda foi recebida com sucesso!!");
+            this.Close();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
